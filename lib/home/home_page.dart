c4479db65c625d13dc8035ffb11fd720154de0fd
erkestkoko <ernesteferetin@yahoo.com>
2021-03-21 22:23:44 +0100

    Added asset and path provider dependencies

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -5,12 +5,42 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: tabs.length, vsync: this);
+  }
+
+  final List<Tab> tabs = <Tab>[
+    Tab(text: 'Zeroth'),
+    Tab(text: 'First'),
+    Tab(text: 'Second'),
+    Tab(text: 'Third'),
+    Tab(text: 'Fourth'),
+    Tab(text: 'Fifth'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Library'),
+          title: Text('Library'),
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: tabs,
+            isScrollable: true,
+          )),
+      body: TabBarView(
+        controller: _tabController,
+        children: tabs.map((Tab tab) {
+          return Center(
+            child: Text("${tab.text} Tab"),
+          );
+        }).toList(),
       ),
     );
   }
